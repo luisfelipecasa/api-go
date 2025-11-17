ea52187f8f121009045f6bc2594bec4087f3d6ab
luis felipe <luisfelipecasagrande878@gmai.com>
2025-11-17 00:12:25 -0300

    criar produtos e procurar produtos por id

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main(){
+func main() {
 
 	server := gin.Default()
 
@@ -25,7 +25,7 @@ func main(){
 	//camada de controllers
 	ProductController := controller.NewProductController(ProductUseCase)
 
-	server.GET("/ping", func(ctx *gin.Context){
+	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
@@ -33,6 +33,8 @@ func main(){
 
 	//cria a rota
 	server.GET("/products", ProductController.GetProducts)
+	server.POST("/products", ProductController.CreateProduct)
+	server.GET("/products/:productId", ProductController.GetProductById)
 
 	server.Run(":8000")
 }
